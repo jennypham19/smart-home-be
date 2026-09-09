e58051f671da71c09e09a7883b33d6fc021f10e4
anhpn <ngocanh.pham19042001@gmail.com>
2026-09-09 17:23:56 +0700

    smart-home-be

---
+++ src/migrations/20260909075751-create-users.js
@@ -0,0 +1,166 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+      // =====================================================
+      // 1. USERS TABLE
+      // =====================================================
+
+      await queryInterface.createTable('Users', {
+        // Cột id: BIGINT tự sinh, là khóa chính
+        id: {
+          type: Sequelize.BIGINT, // BIGINT UNSIGNED là dùng trong MySQL, nhưng Sequelize không có kiểu dữ liệu UNSIGNED, nên dùng BIGINT bình thường
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true
+        },
+        // full_name: Họ tên đầy đủ, Varchar(150), không được null
+        full_name: {
+          type: Sequelize.STRING(150),
+          allowNull: false
+        },
+        // phone: Số điện thoại - định danh đăng nhập chính, Varchar(20), không được null, duy nhất
+        phone: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        // email: Email, Varchar(255), có thể null, duy nhất khi khác NULL
+        email: {
+          type: Sequelize.STRING(255),
+          allowNull: true,
+        },
+        // password: Mật khẩu, Varchar(255), null nếu chỉ đăng nhập OTP, không null nếu đăng nhập bằng mật khẩu, bcrypt cost >= 12
+        password: {
+          type: Sequelize.STRING(255),
+          allowNull: true
+        },
+        // avatar_url: Đường dẫn ảnh đại diện, varchar(500), có thể null
+        avatar_url: {
+          type: Sequelize.STRING(500),
+          allowNull: true
+        },
+        // gender: Giới tính, ENUM('male', 'female', 'other'), có thể null
+        gender: {
+            type: Sequelize.ENUM('male', 'female', 'other'),
+            allowNull: true
+        },
+        // date_of_birth: Ngày sinh (phục vụ ưu đãi sinh nhật), DATE, có thể null
+        date_of_birth: {
+          type: Sequelize.DATE,
+          allowNull: true
+        },
+        // role: Vai trò trong hệ thống, ENUM (‘customer’, ‘affiliate’, ‘staff’, ‘admin’), không được null, mặc định là ‘customer’, index
+        role: {
+          type: Sequelize.ENUM('customer', 'affiliate', 'staff', 'admin'),
+          allowNull: false,
+          defaultValue: 'customer'
+        },
+        // loyalty_points: Số dư điểm hiện tại (denormalize từ loyalty_transactions), INT UNSIGNED, không được null, mặc định là 0
+        loyalty_points: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 0
+        },
+        // total_spent: Tổng chi tiêu (phục vụ phân hạng khách hàng), DECIMAL(15,2), không được null, mặc định là 0.00
+        total_spent: {
+          type: Sequelize.DECIMAL(15,2),
+          allowNull: false,
+          defaultValue: 0.00
+        },
+        // status: Trạng thái tài khoản, ENUM('active', 'inactive', 'blocked'), không được null, mặc định là 'active', index
+        status: {
+          type: Sequelize.ENUM('active', 'inactive', 'blocked'),
+          allowNull: false,
+          defaultValue: 'active'
+        },
+        // email_verified_at: Thời điểm xác thực email, DATE, có thể null
+        email_verified_at: {
+            type: Sequelize.DATE,
+            allowNull: true
+        },
+        // failed_login_account: Số lần đăng nhập sai liên tiếp, khóa tài khoản tại 5, TINYINT UNSIGNED, không được null, mặc định là 0
+        failed_login_account: {
+            type: Sequelize.SMALLINT,
+            allowNull: false,
+            defaultValue: 0
+        },
+        // locked_until: Thời điểm mở khóa tài khoản, DATETIME, có thể null
+        locked_until: {
+            type: Sequelize.DATE,
+            allowNull: true
+        },
+        // last_login_at: Thời điểm đăng nhập gần nhất, DATETIME, có thể null
+        last_login_at: {
+            type: Sequelize.DATE,
+            allowNull: true
+        },
+        // created_at: Thời điểm tạo bản ghi, DATETIME, không được null, mặc định là thời điểm hiện tại, CURRENT_TIMESTAMP 
+        created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        },
+        // updated_at: Thời điểm cập nhật bản ghi gần nhất, DATETIME, không được null, mặc định là thời điểm hiện tại, CURRENT_TIMESTAMP ON UPDATE 
+        updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') // Lưu ý: Sequelize không hỗ trợ trực tiếp 'ON UPDATE CURRENT_TIMESTAMP', nên cần xử lý trong ứng dụng hoặc trigger
+        },
+        // deleted_at: Thời điểm xóa mềm bản ghi, DATETIME, có thể null
+        deleted_at: {
+            type: Sequelize.DATE,
+            allowNull: true
+        }
+      })
+      // =====================================================
+      // 2. UNIQUE INDEXES
+      // =====================================================
+      await queryInterface.addIndex('Users', ['phone'], {
+        name: 'uk_users_phone',
+        unique: true
+      });
+
+      await queryInterface.addIndex('Users', ['email'], {
+        name: 'uk_users_email',
+        unique: true
+      });
+
+      // =====================================================
+      // 3. INDEXES
+      // =====================================================     
+      await queryInterface.addIndex('Users', ['role', 'status'], {
+        name: 'idx_users_role_status'
+      });
+
+      await queryInterface.addIndex('Users', ['deleted_at'], {
+        name: 'idx_users_deleted'
+      });
+  
+      // =====================================================
+      // 4. CHECK CONSTRAINTS
+      // =====================================================
+      await queryInterface.sequelize.query(`
+        ALTER TABLE "Users"
+        ADD CONSTRAINT "chk_users_points"
+        CHECK (loyalty_points >= 0)  
+      `)
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Users');
+  }
+};
